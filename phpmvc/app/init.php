6aa81a9358e85d4f1be1a1aa8d1a852768cc51e7
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-10-14 05:19:22 +0700

    mengelola assets

--- phpmvc/app/init.php
+++ phpmvc/app/init.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+
+
+require_once 'core/Constants.php';
--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,1 +1,9 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya</h1>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at justo nibh. Vivamus accumsan venenatis interdum.</p>
+    <hr class="my-4">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum pretium massa, in malesuada tortor molestie at.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </div>
+</div>
